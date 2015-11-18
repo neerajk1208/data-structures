2cc22b79a7bda24efd33dc50b57cbb6510374686
neerajk1208 <neerajk1208@gmail.com>
2015-11-18 15:11:57 -0800

    Completed Stack

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,30 @@
 var Stack = function() {
   var someInstance = {};
-
+  var stackSize = 0;
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    storage[stackSize] = value;
+    stackSize++;
   };
 
   someInstance.pop = function() {
+    //Create local variable that will store last property
+    debugger;
+    var deleted = storage[stackSize - 1];
+    //Delete the property
+    delete storage[stackSize - 1];
+    stackSize--;
+    //return local variable
+    return deleted;
   };
 
   someInstance.size = function() {
-  };
+    return stackSize>0 ? stackSize : 0;
 
+  };
+  
   return someInstance;
 };
