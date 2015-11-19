6289aee580a5733fca822592b9bed49f7fbb39f6
neerajk1208 <neerajk1208@gmail.com>
2015-11-18 21:11:04 -0800

    Completed all except for Stack Psuedoclassical

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,32 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(queueMethods)
+  someInstance.storage = {};
+  someInstance.qSize = 0;
+  return someInstance;
 };
 
-var queueMethods = {};
-
+var queueMethods = {
+  size: function() {
+    return this.qSize > 0 ? this.qSize : 0;
+  },
+  enqueue: function(data) {
+    this.storage[this.qSize] = data;
+    this.qSize++;
+  },
+  dequeue: function() {
+    var deleted = this.storage[0];
+    //remove the first
+    delete this.storage[0];
+    //storage
+    //change the order and reassign values
+    for (var i = 0; i<this.qSize-1; i++) {
+      this.storage[i] = this.storage[i+1];
+    }
+    this.qSize--;
+    return deleted;
+  
+  }  
+};
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,21 +1,31 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
- /* var someInstance = Object.create(Stack.prototype);
-
-  var stackSize = 0;
-  var storage = {};
-  
+  var someInstance = Object.create(stackMethods);
+  someInstance.storage = {};
+  someInstance.stackSize = 0;
   return someInstance;
 };
 
-//1. how do we incorporate stackMethods and prototypes?
-
-*/
-}
 var stackMethods = {
-  
-  
+  size: function() {
+    return this.stackSize > 0 ? this.stackSize : 0;
+  },
+  push: function(data) {
+    this.storage[this.stackSize] = data;
+    this.stackSize++;
+  },
+  pop: function() {
+    var deleted = this.storage[this.stackSize-1]
+    delete this.storage[this.stackSize-1]
+    this.stackSize--;
+    return deleted;
+   }  
 };
 
 
+
+
+// someInstance.stackSize = 0;
+  //var storage = {};
+  
