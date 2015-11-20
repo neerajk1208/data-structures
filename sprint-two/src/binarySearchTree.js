6a081f677d2b50597926beb0fb3dc963d2c40d14
neerajk1208 <neerajk1208@gmail.com>
2015-11-20 12:48:48 -0800

    Completed two tests on binarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,51 @@
 var BinarySearchTree = function(value) {
+    var BST = Object.create(searchMethods)
+    BST.value = value;
+    BST.left = undefined;
+    BST.right = undefined;
+    return BST;
 };
 
+var searchMethods = {
+  insert: function(value) {    
+    //if value > this.value
+      //we're going right
+      //if this.value's right prop is null
+        //insert newNode
+           //if input value is greater than value at node
+    var check = function(obj) {
+      if (value > obj.value) {
+    
+          if (obj.right !== undefined) {
+            check(obj.right)
+          } else {
+            var newNode = BinarySearchTree(value);
+            obj.right = newNode;
+          }
+      } else if (value < obj.value) {
+        if (obj.left !== undefined) {
+           check(obj.left)
+        } else {
+          var newNode = BinarySearchTree(value)
+          obj.left = newNode;
+        }
+      }
+    }
+    check(this);
+},
 
+contains: function(value) {
+  return 0;
+  
+},
+
+
+
+depthFirstLog: function(first_argument) {
+  return 0;
+}
+
+}
 /*
  * Complexity: What is the time complexity of the above functions?
  */
